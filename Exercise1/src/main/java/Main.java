b0cda61abaf7f970418e8ddd162b3f622d36ff7b
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-19 16:09:50 +0200

    refactor: change argumennt for calculation demonstrationn

--- Exercise1/src/main/java/Main.java
+++ Exercise1/src/main/java/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         Worker w1 = new Worker("Jhon", "Jhonson", 40.6);
-        OnlineWorker w2 = new OnlineWorker("Samuel", "L. Jackson", 78.3);
-        PresentWorker w3 = new PresentWorker("Diego", "De la Vega", 100);
+        OnlineWorker w2 = new OnlineWorker("Samuel", "L. Jackson", 40.6);
+        PresentWorker w3 = new PresentWorker("Diego", "De la Vega", 40.6);
 
         System.out.println(w1.getName()+"'s salary is:" + w1.calculateSalary(100));
 
