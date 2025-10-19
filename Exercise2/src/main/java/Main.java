959422547a3c91468aeb6d2e6b56f1f7c54edd8d
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-19 16:52:30 +0200

    test: test deprecated method

--- Exercise2/src/main/java/Main.java
+++ Exercise2/src/main/java/Main.java
@@ -5,16 +5,19 @@ public class Main {
         OnlineWorker w2 = new OnlineWorker("Samuel", "L. Jackson", 40.6);
         PresentWorker w3 = new PresentWorker("Diego", "De la Vega", 40.6);
 
-        System.out.println(w1.getName()+"'s salary is:" + w1.calculateSalary(100)+"euro");
+        System.out.println(w1.getName()+"'s salary is:" + w1.calculateSalary(100)+" euro");
 
-        System.out.println(w2.getName()+"'s salary is:" + w2.calculateSalary(100)+"euro");
+        System.out.println(w2.getName()+"'s salary is:" + w2.calculateSalary(100)+" euro");
 
-        System.out.println(w3.getName()+"'s salary is:" + w3.calculateSalary(100)+"euro");
+        System.out.println(w3.getName()+"'s salary is:" + w3.calculateSalary(100)+" euro");
 
 
         @SuppressWarnings("deprecation")
         double salaryPecetas = w3.calculateSalaryPecetas(100);
-        System.out.println(w3.getName()+"'s salary is:" + salaryPecetas+"peceta");
+        System.out.println(w3.getName()+"'s salary is:" + salaryPecetas+" peceta");
+
+        double lightCost = w2.calculateLightCost(100, 0.1);
+        System.out.println(w2.getName()+"'s light cost is:" + lightCost+" euro");
 
     }
 }
