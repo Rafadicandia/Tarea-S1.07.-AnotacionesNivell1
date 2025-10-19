1c88e91a63694b8b750ec666223d7a8e646804c7
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-19 15:28:28 +0200

    build: creaetes worker class with  fields and constructor

---
+++ Exercise1/src/main/java/Worker.java
@@ -0,0 +1,31 @@
+public abstract class Worker {
+
+    private String name;
+    private String surname;
+    private float hourlyRate;
+
+public Worker(String name, String surname, float hourlyRate){
+    if (name==(null)){
+        throw new IllegalArgumentException("The name of the product can't be null");
+    }
+
+    if (name.isBlank()){
+        throw new IllegalArgumentException("The name of the product can't be empty");
+    }
+
+    if (surname==(null)){
+        throw new IllegalArgumentException("The name of the product can't be null");
+    }
+
+    if (surname.isBlank()){
+        throw new IllegalArgumentException("The name of the product can't be empty");
+    }
+
+    if (hourlyRate<0){
+        throw new IllegalArgumentException("Hourly rate can't be negative");
+    }
+    this.name = name;
+    this.surname = surname;
+    this.hourlyRate = hourlyRate;
+}
+}
